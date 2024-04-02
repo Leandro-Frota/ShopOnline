f2ae4359da2b04297c2aeeb935c590c9371db6c3
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2024-04-01 23:10:56 -0300

    Add features buy cart

--- src/app/page.js
+++ src/app/page.js
@@ -22,28 +22,35 @@ export default function Home() {
   setProducts(prod)
  }
 
+ function addCart(product){
+
+    setCart([...cart,product])
+ }
+
   return (
     <main className="flex min-h-screen max-w-screen flex-wrap gap-6 items-center justify-center p-24">
       <div className="min-w-full flex justify-end px-12">
         <div className="cursor-pointer hover:scale-125 transition-all relative">
           <Image src={cartImage} width={50} height={50} />
-          <span className="absolute bottom-1  bg-red-500 flex justify-center items-center rounded-full text-[18px] w-[24px] h-[24px]">0</span>
+          <span className="absolute bottom-1  bg-red-500 flex justify-center items-center rounded-full text-[12px] w-[24px] h-[24px]">{cart.length}</span>
         </div>
       </div>
       {products.map((p) => {
-        return (<div key={p.id} className="rounded-md border-2 border-black
-         p-6 flex justify-center flex-col items-center gap-4 max-w-48 min-h-[400px] ">
-          <Image src={p.image} width={100} height={100} alt={p.title} className="max-h-20 object-contain"/>
-          <div className="text-center">
-            {p.title.slice(0,15)}...
+        return (
+          <div key={p.id} className="rounded-md border-2 border-black
+          p-6 flex justify-center flex-col items-center gap-4 max-w-48 min-h-[400px] ">
+            <Image src={p.image} width={100} height={100} alt={p.title} className="max-h-20 object-contain"/>
+            <div className="text-center">
+              {p.title.slice(0,15)}...
+            </div>
+            <div className="text-xl font-bold">
+              R$ {p.price}
+            </div>
+            <div className="flex flex-grow"></div>
+            <button className="hover:bg-red-700 transition-all bg-red-500 text-white p-2 rounded min-w-full">COMPRAR</button>
+            <button onClick={()=>addCart(p)} className="hover:bg-cyan-700 transition-all bg-cyan-500 text-white p-2 rounded min-w-full">ADICIONAR AO CARRINHO</button>
           </div>
-          <div className="text-xl font-bold">
-            R$ {p.price}
-          </div>
-          <div className="flex flex-grow"></div>
-          <button className="hover:bg-red-700 transition-all bg-red-500 text-white p-2 rounded min-w-full">COMPRAR</button>
-          <button className="hover:bg-cyan-700 transition-all bg-cyan-500 text-white p-2 rounded min-w-full">ADICIONAR AO CARRINHO</button>
-        </div>)
+        )
       })}
     </main>
   );
